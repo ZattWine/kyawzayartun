71d5466bab9a5cb1a5fc156b42d5416dc2ec38da
Zayar Tun <kyawzayartun.dev@gmail.com>
2021-06-27 19:04:31 +0630

    fixed wrong text and change brand text size

--- components/Header.js
+++ components/Header.js
@@ -3,14 +3,14 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <header className='flex flex-col h-20 text-center items-center rounded-2xl bg-white text-gray-700 border-2 border-gray-100 m-4 pt-4 md:flex-row md:h-14 md:justify-between md:items-center md:space-x-4 md:px-7 md:py-2 md:m-4'>
-      <div className='uppercase text-indigo-500 text-base md:text-xl font-bold'>
+    <header className='flex flex-col text-center items-center rounded-2xl bg-white text-gray-700 m-4 pt-4 md:flex-row md:justify-between md:items-center md:space-x-4 md:px-7 md:py-2 md:m-4'>
+      <div className='uppercase text-indigo-700 text-5xl font-bold'>
         <Link href='/'>
           <a>{getPeriodTime()}</a>
         </Link>
       </div>
       <nav>
-        <ul className='flex flex-row space-x-4'>
+        <ul className='flex flex-row space-x-4 text-xl mt-2 md:mt-0'>
           <li>
             <Link href='/'>
               <a className='hover:text-indigo-500'>Home</a>
--- data/me.js
+++ data/me.js
@@ -4,7 +4,7 @@ export const me = {
   lastname: 'Tun',
   fullname: 'Kyaw Zayar Tun',
   avatar: '/images/shwedagon-me.jpg',
-  intro: `Hi! I'm Kyaw Zayar Tun. I got a bachelor degree of Computer Science (B.C.Sc) from the University of Computer Studies in 2019, Yangon. I'm an Android developer. I'm also able to build normal web applications and I enjoy to learn new technologies.`,
+  intro: `Hi! I'm Kyaw Zayar Tun. I got a bachelor degree of Computer Science (B.C.Sc) from the University of Computer Studies, Yangon in 2019. I'm an Android developer. I'm also able to build normal web applications and I enjoy to learn new technologies.`,
   sociallinks: {
     twitter: 'https://twitter.com/ZattWine',
     facebook: 'https://www.facebook.com/zy.98727',
